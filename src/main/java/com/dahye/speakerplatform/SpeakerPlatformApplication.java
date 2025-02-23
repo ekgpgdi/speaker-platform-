c4b72472709a76854adae75bb498c67ff81330ed
ekgpgdi <dlekgp0423@naver.com>
2025-02-23 23:17:46 +0900

    🎉 init commit

---
+++ src/main/java/com/dahye/speakerplatform/SpeakerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dahye.speakerplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakerPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakerPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dahye/speakerplatform/SpeakerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahye.speakerplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakerPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
