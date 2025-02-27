48607fbd998aab1ed5a388e1c468fb0d0a5a46da
ekgpgdi <dlekgp0423@naver.com>
2025-02-27 11:11:14 +0900

    feat : 강연 신청 관리를 위한 redis 설정 추가

---
+++ src/main/java/com/dahye/speakerplatform/common/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.dahye.speakerplatform.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    // 첫 번째 RedisTemplate: 강연 좌석 관리용
+    @Bean(name = "lectureSeatsRedisTemplate")
+    public RedisTemplate<String, String> lectureSeatsRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashValueSerializer(RedisSerializer.string());
+        return redisTemplate;
+    }
+
+    // 두 번째 RedisTemplate: 유저 신청 관리용
+    @Bean(name = "userApplicationRedisTemplate")
+    public RedisTemplate<String, String> userApplicationRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashValueSerializer(RedisSerializer.string());
+        return redisTemplate;
+    }
+}
