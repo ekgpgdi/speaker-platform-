407cbb6ba84764e69fd77c66dca48400a9d8d076
ekgpgdi <dlekgp0423@naver.com>
2025-02-27 12:53:30 +0900

    test : 강연 생성 시 강연 신청 가능 좌석, 시간 redis 에 저장 코드에 따른 테스트 코드 수정

--- src/test/java/com/dahye/speakerplatform/lectures/service/AdminLectureServiceCreateTest.java
+++ src/test/java/com/dahye/speakerplatform/lectures/service/AdminLectureServiceCreateTest.java
@@ -12,6 +12,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,16 +24,30 @@ public class AdminLectureServiceCreateTest {
     @Mock
     LectureRepository lectureRepository;
 
+    @Mock
+    LectureApplicationService lectureApplicationService;
+
     @Test
     @DisplayName("[ADMIN] 강연 생성 성공 테스트")
     public void testLectureCreate_Success() {
         // Given
         LectureCreateRequest lectureCreateRequest = new LectureCreateRequest();
         lectureCreateRequest.setLecturer("박다솔");
-        lectureCreateRequest.setLocation("경기도 화성시 남여울2길 4");
+        lectureCreateRequest.setLocation("경기 화성");
         lectureCreateRequest.setCapacity(6);
         lectureCreateRequest.setStartTime("2025-06-01 10:00:00");
         lectureCreateRequest.setContent("체어 + 바렐 : 강의실에 오시면 당일 진행 방식(체어 또는 바렐)을 안내해 드립니다.");
+
+        Lecture mockLecture = Lecture.builder()
+                .id(1L)
+                .lecturer("박다솔")
+                .location("경기 화성")
+                .capacity(6)
+                .startTime(LocalDateTime.parse("2025-06-01T10:00:00"))
+                .content("체어 + 바렐 : 강의실에 오시면 당일 진행 방식(체어 또는 바렐)을 안내해 드립니다.")
+                .build();
+
+        Mockito.when(lectureRepository.save(Mockito.any(Lecture.class))).thenReturn(mockLecture);
 
         // When
         ResponseCode responseCode = lectureService.createLecture(lectureCreateRequest);
@@ -39,5 +55,7 @@ public class AdminLectureServiceCreateTest {
         // Then
         assertEquals(ResponseCode.CREATED, responseCode);
         Mockito.verify(lectureRepository, Mockito.times(1)).save(Mockito.any(Lecture.class));
+
+        Mockito.verify(lectureApplicationService, Mockito.times(1)).createLectureRedis(mockLecture);
     }
 }
